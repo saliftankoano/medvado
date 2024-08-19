260c399804032e419483578cccbab242c8bbab97
Salif Tankoano <tanksalif@gmail.com>
2024-08-19 15:16:24 -0400

    remove flowbite script

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,9 +16,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <head>
-        <script src="../path/to/flowbite/dist/flowbite.min.js"></script>
-      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
